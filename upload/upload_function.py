aaef73e36f9d616215d414673cf39a2ebaf6a6a1
inspirit941 <inspirit941@gmail.com>
2023-07-06 21:25:11 +0900

    fix: upload api에서 cors block 해소

--- upload/upload_function.py
+++ upload/upload_function.py
@@ -1,12 +1,24 @@
 import functions_framework
 import os
 import random, string
-from google.cloud import storage
-
+from google.cloud import storage, firestore
 
 
 @functions_framework.http
 def hello_http(request):
+    # Set CORS headers for the preflight request
+    if request.method == "OPTIONS":
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Max-Age": "3600",
+        }
+        return ("", 204, headers)
+
     f = request.files['file']
     random_string = ''.join(random.choices(string.ascii_lowercase, k=5))
     filedir = os.path.join(os.getcwd(), random_string)
@@ -19,10 +31,19 @@ def hello_http(request):
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(BUCKET_NAME)
 
-    blob = bucket.blob("-".join(random_string, f.filename))
+    bucket_filename = "-".join([random_string, f.filename])
+    blob = bucket.blob(bucket_filename)
     blob.upload_from_filename(filepath)
 
-    return {
-        'statusCode': 200,
-        'body': filepath
-    }
+    db = firestore.Client()
+    doc_ref = db.collection("voice_to_text").document(bucket_filename)
+    doc_ref.set({
+        'name': bucket_filename,
+        'progress': "Speech to Text 변환 중..."
+    })
+    headers = {"Access-Control-Allow-Origin": "*"}
+    return (
+        {"body": bucket_filename},
+        200,
+        headers
+    )
